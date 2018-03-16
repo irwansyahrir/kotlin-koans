ec51ce99d08db7be41ac06be9339bd63f766227c
irwansyahrir <irwan.syahrir@gmail.com>
2018-03-16 21:35:09 +0100

    Learned Compound Task and Extensions on Collections

--- src/ii_collections/_23_CompoundTasks.kt
+++ src/ii_collections/_23_CompoundTasks.kt
@@ -8,11 +8,13 @@ fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    return orders.fold()
+    return orders.filter { it.isDelivered }.flatMap { it.products }.toSet().maxBy { it.price }
+
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return customers.flatMap { it.orders }.toList()
+            .flatMap { it.products }.toList().filter { it == product }.size
 }
