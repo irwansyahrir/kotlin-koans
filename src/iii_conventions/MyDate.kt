c07268e18b6a37336c6e1c1db4e7ca658be3ce76
irwansyahrir <irwan.syahrir@gmail.com>
2018-04-26 07:33:59 +0200

    28 Learning For Loop

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -6,6 +6,10 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparabl
         month != other.month -> month - other.month
         else -> dayOfMonth - other.dayOfMonth
     }
+
+    operator fun inc(): MyDate {
+        return this.nextDay()
+    }
 }
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange {
@@ -22,4 +26,21 @@ class DateRange(val start: MyDate, val endInclusive: MyDate) {
     operator fun contains(d: MyDate): Boolean {
         return start <= d && d <= endInclusive
     }
+
+    operator fun iterator(): Iterator<MyDate> {
+        return DateRangeIterator(start, endInclusive)
+    }
+}
+
+class DateRangeIterator(start: MyDate, private val endInclusive: MyDate) : Iterator<MyDate> {
+    private var current = start
+
+    override fun hasNext(): Boolean {
+        return current <= endInclusive
+    }
+
+    override fun next(): MyDate {
+        return current++
+    }
+
 }
