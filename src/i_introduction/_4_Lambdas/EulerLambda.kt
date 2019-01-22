c91210c0860e9d0ab325aac542fbf5780f531d0b
Irwan Syahrir <irwan.syahrir@sparebank1.no>
2019-01-22 16:13:43 +0100

    Use lambda to solve a project euler problem - fibonacci

--- src/i_introduction/_4_Lambdas/EulerLambda.kt
+++ src/i_introduction/_4_Lambdas/EulerLambda.kt
@@ -3,11 +3,11 @@ package i_introduction._4_Lambdas
 fun numbersDivisibleByThreeOrFiveBelow(top: Int): List<Int> {
     val divisibles : MutableList<Int> = mutableListOf()
 
-    val isRemBy3 : (Int) -> Boolean =  { it.rem(3) == 0}
-    val isRemBy5 : (Int) -> Boolean =  { it.rem(5) == 0}
+    val mod3 : (Int) -> Boolean =  { it.rem(3) == 0}
+    val mod5 : (Int) -> Boolean =  { it.rem(5) == 0}
 
     for (i in 1..(top-1)) {
-        if (isRemBy3(i) || isRemBy5(i)) {
+        if (mod3(i) || mod5(i)) {
             divisibles.add(i)
         }
     }
@@ -17,4 +17,35 @@ fun numbersDivisibleByThreeOrFiveBelow(top: Int): List<Int> {
 
 fun sumOfNumbersDivisibleByThreeOrFiveBelow(topLimit: Int): Int {
     return numbersDivisibleByThreeOrFiveBelow(topLimit).sum()
+}
+
+fun fibo(x: Int): List<Int> {
+    val fibos : MutableList<Int> = mutableListOf()
+    for (i in 1..x) {
+        if (i <= 2) {
+            fibos.add(i)
+        } else {
+            fibos.add(fibos.get(i-2) + fibos.get(i-3))
+        }
+    }
+    return fibos
+}
+
+fun maxFibo(x: Int): Int {
+    return fibo(x).last()
+}
+
+
+fun numOfTermForValueBelow(x: Int): Int {
+    var seed = 1
+
+    while (maxFibo(seed) < x) {
+        seed += 1
+    }
+
+    return seed -1
+}
+
+fun sumOfEvenTermsForFibo(x: Int): Int {
+    return fibo(x).filter { f -> f.rem(2) == 0  }.sum()
 }
--- test/i_introduction/_4_Lambdas/EulerLambdaTest.kt
+++ test/i_introduction/_4_Lambdas/EulerLambdaTest.kt
@@ -15,4 +15,45 @@ class EulerLambdaTest {
         assertEquals(23, sumOfNumbersDivisibleByThreeOrFiveBelow(10))
         assertEquals(233168, sumOfNumbersDivisibleByThreeOrFiveBelow(1000))
     }
+
+    @Test
+    fun generateFibonacci() {
+        assertEquals(listOf(1), fibo(1))
+        assertEquals(listOf(1, 2), fibo(2))
+        assertEquals(listOf(1, 2, 3), fibo(3))
+        assertEquals(listOf(1, 2, 3, 5), fibo(4))
+        assertEquals(listOf(1, 2, 3, 5, 8), fibo(5))
+        assertEquals(listOf(1, 2, 3, 5, 8, 13, 21, 34, 55, 89), fibo(10))
+    }
+
+    @Test
+    fun getMaxFiboValue() {
+        assertEquals(1, maxFibo(1))
+        assertEquals(2, maxFibo(2))
+        assertEquals(3, maxFibo(3))
+        assertEquals(5, maxFibo(4))
+        assertEquals(89, maxFibo(10))
+        assertEquals(3524578, maxFibo(32))
+        assertEquals(5702887, maxFibo(33))
+    }
+
+    @Test
+    fun getNumberOfTermsForMaxValue() {
+        assertEquals(0, numOfTermForValueBelow(1))
+        assertEquals(1, numOfTermForValueBelow(2))
+        assertEquals(2, numOfTermForValueBelow(3))
+        assertEquals(3, numOfTermForValueBelow(5))
+        assertEquals(9, numOfTermForValueBelow(89))
+        assertEquals(32, numOfTermForValueBelow(4000000))
+    }
+
+    @Test
+    fun getSumOfEvenTerms() {
+        assertEquals(0, sumOfEvenTermsForFibo(1))
+        assertEquals(2, sumOfEvenTermsForFibo(2))
+        assertEquals(2, sumOfEvenTermsForFibo(3))
+        assertEquals(2, sumOfEvenTermsForFibo(4))
+        assertEquals(10, sumOfEvenTermsForFibo(5))
+        assertEquals(4613732, sumOfEvenTermsForFibo(32))
+    }
 }
