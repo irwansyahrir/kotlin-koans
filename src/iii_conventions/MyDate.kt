fa25fb8285d7e8c934919871c0d81951a039d095
Irwan Syahrir <irwan.syahrir@sparebank1.no>
2019-04-08 02:16:09 +0200

    More operator overloading. This is fun, look how I changed the naming so that the calling method is more readable.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -22,11 +22,26 @@ operator fun MyDate.inc(): MyDate {
     return this.nextDay()
 }
 
+operator fun MyDate.plus(interval: TimeInterval): MyDate {
+    return this.addTimeIntervals(interval, 1)
+}
+
+operator fun MyDate.plus(repeated: RepeatedTimeInterval): MyDate {
+    return this.addTimeIntervals(repeated.interval, repeated.times)
+}
+
 enum class TimeInterval {
     DAY,
     WEEK,
     YEAR
 }
+
+operator fun TimeInterval.times(n: Int): RepeatedTimeInterval {
+    return RepeatedTimeInterval(this, n)
+}
+
+class RepeatedTimeInterval(val interval: TimeInterval, val times: Int)
+
 
 class DateRange(val start: MyDate, val endInclusive: MyDate): Iterable<MyDate> {
     override fun iterator(): Iterator<MyDate> {
